83112b4bc068fe9547484a64a8962b236b48f2e3
jack <jrfisch95@gmail.com>
2022-10-31 06:48:35 -0400

    update note in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     hmr: {
-      clientPort: 3050, //this is whatever port our NGINX proxy is using on our local machine
+      clientPort: 3050, //this is whatever port our docker-compose is using to bind to our NGINX proxy (running on port 80 in a docker container) on our local machine
       path: '/ws'
     }
   }
